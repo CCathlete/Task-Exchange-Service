b016bc3bb11ca5142be4997de0356e4265536a49
CCat <ccathlete01@gmail.com>
2024-10-21 15:48:26 +0200

    Added CreateAccountingRecord in db.go. I need to add Get and Update functions for accounting records and add migration using GORM that will create the tables automatically from my Go entities.

--- infrastructure/db.go
+++ infrastructure/db.go
@@ -11,6 +11,10 @@ import (
 	_ "github.com/lib/pq"
 )
 
+/*
+TODO: Make sure that the task table has task_id for each row and not just id.
+Same with users table - user_id and accounting_records tables - record_id.
+*/
 func InitDB(config Config) (*sql.DB, error) {
 	connectStringNoDB := fmt.Sprintf(
 		"host=%s port=%d user=%s password=%s sslmode=%s",
@@ -19,7 +23,7 @@ func InitDB(config Config) (*sql.DB, error) {
 
 	db, err := sql.Open("postgres", connectStringNoDB)
 	if err != nil {
-		return nil, fmt.Errorf("error connecting to postgres server: %v", err)
+		return nil, fmt.Errorf("error connecting to postgres server: %w", err)
 	}
 	defer db.Close()
 
@@ -29,14 +33,14 @@ func InitDB(config Config) (*sql.DB, error) {
 	query := fmt.Sprintf("SELECT EXISTS(SELECT 1 FROM pg_database WHERE datname = '%s'", config.DBName)
 	err = db.QueryRow(query).Scan(&itExists)
 	if err != nil {
-		return nil, fmt.Errorf("error checking if database exists: %v", err)
+		return nil, fmt.Errorf("error checking if database exists: %w", err)
 	}
 
 	// We create the DB if it doesn't exist.
 	if !itExists {
 		_, err = db.Exec(fmt.Sprintf("CREATE DATABASE %s", config.DBName))
 		if err != nil {
-			return nil, fmt.Errorf("couldn't create the database: %v", err)
+			return nil, fmt.Errorf("couldn't create the database: %w", err)
 		}
 		log.Printf("Database %s created.\n", config.DBName)
 	} else {
@@ -48,11 +52,11 @@ func InitDB(config Config) (*sql.DB, error) {
 
 	db, err = sql.Open("postgres", connectStringWithDB)
 	if err != nil {
-		return nil, fmt.Errorf("error connecting to target database: %v", err)
+		return nil, fmt.Errorf("error connecting to target database: %w", err)
 	}
 
 	if err := db.Ping(); err != nil {
-		return nil, fmt.Errorf("connection was successful but DB is not responding: %v", err)
+		return nil, fmt.Errorf("connection was successful but DB is not responding: %w", err)
 	}
 
 	return db, nil
@@ -85,11 +89,11 @@ func UpdateTask(db *sql.DB, description, status string, taskID, assignedTo int, 
 			assigned_to = $3,
 			price = $4,
 			is_completed = $5
-		WHERE id = $6
+		WHERE task_id = $6
 		`
 	_, err := db.Exec(query, description, status, assignedTo, price, isCompleted, taskID)
 	if err != nil {
-		return fmt.Errorf("failed to update task: %v", err)
+		return fmt.Errorf("failed to update task: %w", err)
 	}
 
 	return nil
@@ -97,7 +101,7 @@ func UpdateTask(db *sql.DB, description, status string, taskID, assignedTo int, 
 
 // Getting tasks that are assigned to a specific user.
 func GetTasks(db *sql.DB, userID int) ([]entities.Task, error) {
-	query := `SELECT id, description, assigned_to, status, price FROM tasks WHERE assigned_to = $1`
+	query := `SELECT task_id, description, assigned_to, status, price FROM tasks WHERE assigned_to = $1`
 	rows, err := db.Query(query, userID)
 	if err != nil {
 		return nil, err
@@ -120,11 +124,11 @@ func CreateUser(db *sql.DB, name, email, role, joinedAt string) (int, error) {
 	query := `
 	INSERT INTO users (name, email, role, joined_at)
 	VALUES ($1, $2, $3, $4)
-	RETURNING id
+	RETURNING user_id
 	`
 	err := db.QueryRow(query, name, email, role, joinedAt).Scan(&userID)
 	if err != nil {
-		return 0, fmt.Errorf("there was an issue with creating the user: %v", err)
+		return 0, fmt.Errorf("there was an issue with creating the user: %w", err)
 	}
 
 	return userID, nil
@@ -138,7 +142,7 @@ func GetUser(db *sql.DB, userIDp *int, name, email, role string) (entities.User,
 
 	if userIDp != nil {
 		query = `
-		SELECT id, name, email, role, joined_at 
+		SELECT user_id, name, email, role, joined_at 
 		FROM users 
 		WHERE id = $1
 		`
@@ -146,7 +150,7 @@ func GetUser(db *sql.DB, userIDp *int, name, email, role string) (entities.User,
 	} else {
 		// If userID is not provided we cross other fields.
 		query = `
-		SELECT id, name, email, role, joined_at 
+		SELECT user_id, name, email, role, joined_at 
 		FROM users 
 		WHERE (name = $1 AND email = $2)
 		OR (email = $2 AND role = $3)
@@ -155,12 +159,39 @@ func GetUser(db *sql.DB, userIDp *int, name, email, role string) (entities.User,
 	}
 
 	if err != nil {
-		return entities.User{}, fmt.Errorf("issue with getting user info from the DB: %v", err)
+		return entities.User{}, fmt.Errorf("issue with getting user info from the DB: %w", err)
 	}
 
 	return user, nil
 }
 
-func UpdateUser(db *sql.DB, userID int, description, status string, assignedTo int, price float64, isCompleted bool) error
+func UpdateUser(db *sql.DB, userID int, name, email, role string) error {
+	query := `
+	UPDATE users
+	SET name = $1, email = $2, role = $3
+	WHERE user_id = $4
+	`
 
-func CreateAccountingRecord(db *sql.DB, userID, taskID int, status string, assignedTo int, price float64, isCompleted bool) (int, error)
+	_, err := db.Exec(query, name, email, role, userID)
+	if err != nil {
+		return fmt.Errorf("failed to update user: %w", err)
+	}
+
+	return nil
+}
+
+func CreateAccountingRecord(db *sql.DB, userID, taskID int, status string, assignedTo int, price float64, isCompleted bool) (int, error) {
+	var recordID int
+
+	query := `
+	INSERT INTO accounting_records (user_id, task_id, status, assigned_to, price, is_completed)
+	VALUES ($1, $2, $3, $4, $5, $6)
+	RETURNING record_id
+	`
+	err := db.QueryRow(query, userID, taskID, status, assignedTo, price, isCompleted).Scan(&recordID)
+	if err != nil {
+		return 0, fmt.Errorf("failed to create an accounting record: %w", err)
+	}
+
+	return recordID, nil
+}
